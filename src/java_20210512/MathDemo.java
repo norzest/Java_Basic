22c63d089faaf77a40d0aecfedf67382a26b4067
영효 심 <dark1203@naver.com>
2021-05-12 15:47:55 +0900

    2021-05-12 object

---
+++ src/java_20210512/MathDemo.java
@@ -0,0 +1,20 @@
+package java_20210512;
+
+public class MathDemo {
+	public static void main(String[] args) {
+		System.out.println(Math.ceil(3.1)); // double
+		System.out.println(Math.floor(3.1)); // double
+		System.out.println(Math.round(3.1)); // long
+		System.out.println(Math.round(3.6)); // long
+		System.out.println(Math.abs(-3.1)); // double
+		System.out.println(Math.max(3, 2)); // integer
+		System.out.println(Math.max(3.1, 2.3)); // double		
+		System.out.println(Math.min(3, 2)); // integer
+		System.out.println(Math.min(3.1, 2.3)); // double
+		System.out.println(Math.sqrt(3)); // double
+		System.out.println(Math.pow(2, 3)); // double
+		System.out.println((int)(Math.random() * 45)); // double
+		
+	}
+	
+}
---
+++ src/java_20210512/ObjectDemo.java
@@ -0,0 +1,54 @@
+package java_20210512;
+
+class Test {
+	String name;
+	
+	Test(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Test other = (Test) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() { return name; }
+}
+
+public class ObjectDemo {
+	public static void main(String[] args) {
+		Test t1 = new Test("asd");
+		Test t2 = new Test("asd");
+
+		System.out.println(t1==t2); // 주소가 달라 false
+		System.out.println(t1.equals(t2)); // 같은 오브젝트를 참조하여 주소가 같아짐
+		// equals 함수를 override할때 hashcode 또한 override 되어 같아짐
+		
+		System.out.println(t1.hashCode());
+		System.out.println(t2.hashCode());
+
+		System.out.println(t1); // override
+
+	}
+}
